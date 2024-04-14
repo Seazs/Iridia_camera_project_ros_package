dea589b209f9e2d30c5124e3b9fdd8ed4609ffd5
AlexBien <alexandre.bienfait@hotmail.com>
2024-04-14 21:57:49 +0200

    Update file paths in Camera_listener.py and Camera_publisher.py

--- src/up_pointing_camera/src/Camera_listener.py
+++ src/up_pointing_camera/src/Camera_listener.py
@@ -3,72 +3,44 @@ import rospy
 from sensor_msgs.msg import Image
 from std_msgs.msg import Empty
 from cv_bridge import CvBridge
+import os
 import cv2
-from ultralytics import YOLO
-from ultralytics.engine.results import Results
 
-
-
-class CameraListener:
+class CameraPublisher:
     def __init__(self):
-        self.image_sub = rospy.Subscriber("image_raw", Image, self.callback, queue_size=1)
-        #self.door_detection_pub = rospy.Publisher("door_detection", , queue_size=1)
-        self.bridge = CvBridge()
-        self.image = None
-        self.yolo = YOLO("/home/alexandre/catkin_ws/src/up_pointing_camera/src/yolov8n.pt")
-        self.classes = self.yolo.names #list of classes
+        try:
+            self.image_pub = rospy.Publisher("image_raw", Image, queue_size=1) # qeueu size of 1 as we are only interested in the most recent image
+            self.bridge = CvBridge()
+            self.cap = cv2.VideoCapture(0)
+            rospy.loginfo("Camera has been started")
+            self.rate = rospy.Rate(30)
+            
+            self.image_received = True
+            self.image_sub = rospy.Subscriber("image_proccesed_confirmation", Empty, self.image_callback, queue_size=1)
+        except Exception as e:
+            rospy.logerr("Error: {}".format(e))
+            rospy.signal_shutdown("Error: {}".format(e))
 
-        self.confirmation_pub = rospy.Publisher("image_proccesed_confirmation", Empty, queue_size=1)
+    def run(self):
+        while not rospy.is_shutdown():
+            ret, frame = self.cap.read()
+            if ret and self.image_received:
+                image_message = self.bridge.cv2_to_imgmsg(frame, encoding="bgr8")
+                self.image_pub.publish(image_message)
+                rospy.loginfo("Image has been published")
+                self.image_received = False
+            self.rate.sleep()
+    
+    def image_callback(self, data):
+        rospy.loginfo("Image has been received")
+        self.image_received = True
         
-    def callback(self, data):
-        cv_image = self.bridge.imgmsg_to_cv2(data, desired_encoding="bgr8")
-        #apply the model
-        result: Results = self.yolo.predict(cv_image, show= False)[0]
-        for obj in result.boxes.cls: #cls is the class of the object
-            detected_class = self.classes[int (obj)]
-            print(detected_class)
-            rospy.loginfo(detected_class)
-            # data_analyzed = {"class": detected_class, "confidence": result.probs.confidence[int(obj)]}
-            # self.door_detection_pub.publish(data_analyzed)
-        
-        self.confirmation_pub.publish(Empty()) 
 
 if __name__ == '__main__':
-    rospy.init_node('camera_listener')
-    rospy.loginfo("Camera Listener Node has been started")
-    camera_listener = CameraListener()
+    rospy.init_node('camera_publisher')
+    rospy.loginfo("Camera Publisher Node has been started")
+    #print working directory
+    rospy.loginfo("Current working directory: {}".format(os.getcwd()))
+    camera_publisher = CameraPublisher()
+    camera_publisher.run()
     rospy.spin()
-
-            
-        
-            
-            
-
-
-    # ca c'est qu'il est possible de faire avec les results
-
-    # Attributes:
-    #     orig_img (numpy.ndarray): Original image as a numpy array.
-    #     orig_shape (tuple): Original image shape in (height, width) format.
-    #     boxes (Boxes, optional): Object containing detection bounding boxes.
-    #     masks (Masks, optional): Object containing detection masks.
-    #     probs (Probs, optional): Object containing class probabilities for classification tasks.
-    #     keypoints (Keypoints, optional): Object containing detected keypoints for each object.
-    #     speed (dict): Dictionary of preprocess, inference, and postprocess speeds (ms/image).
-    #     names (dict): Dictionary of class names.
-    #     path (str): Path to the image file.
-
-    # Methods:
-    #     update(boxes=None, masks=None, probs=None, obb=None): Updates object attributes with new detection results.
-    #     cpu(): Returns a copy of the Results object with all tensors on CPU memory.
-    #     numpy(): Returns a copy of the Results object with all tensors as numpy arrays.
-    #     cuda(): Returns a copy of the Results object with all tensors on GPU memory.
-    #     to(*args, **kwargs): Returns a copy of the Results object with tensors on a specified device and dtype.
-    #     new(): Returns a new Results object with the same image, path, and names.
-    #     plot(...): Plots detection results on an input image, returning an annotated image.
-    #     show(): Show annotated results to screen.
-    #     save(filename): Save annotated results to file.
-    #     verbose(): Returns a log string for each task, detailing detections and classifications.
-    #     save_txt(txt_file, save_conf=False): Saves detection results to a text file.
-    #     save_crop(save_dir, file_name=Path("im.jpg")): Saves cropped detection images.
-    #     tojson(normalize=False): Converts detection results to JSON format.
