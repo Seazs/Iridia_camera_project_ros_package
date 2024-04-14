f94fae82c089bd56f5da7b4cf40ad47964a0bf3a
Seazs <alexandre.bienfait@hotmail.com>
2024-04-14 22:50:58 +0200

    Remove unnecessary files and folders

--- build/up_pointing_camera/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/alexandre/old_catkin_ws/src/up_pointing_camera
-# Build directory: /home/alexandre/old_catkin_ws/build/up_pointing_camera
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
